dcabed10321655df78088f3b9719d211ba161e54
thofik93 <thofikwiranata@ymail.com>
2015-08-27 11:25:51 +0700

    bagian header aplikasi

---
+++ _header.php
@@ -0,0 +1,39 @@
+<div class="row page-header">
+	<div class="col-md-3 col-sm-12 col-xs-12">
+		<a href="?page=beranda"><img src="images/logo.png" alt="logo" /></a>
+	</div>
+
+	<div class="col-md-7 col-sm-9 col-xs-9">
+		<ul class="nav nav-pills">
+			<li><a href="?page=beranda" class="text-white"><span class="glyphicon glyphicon-home"></span>&nbsp;Beranda</a></li>
+			<li class="dropdown">
+				<a href="#" data-toggle="dropdown" data-target="#" class="text-white">
+					Daerah<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+					<li><a href="?page=serang">Serang</a></li>
+					<li><a href="?page=tangerang">Tangerang</a></li>
+					<li><a href="?page=cilegon">Cilegon</a></li>
+				</ul>
+			</li>
+			<li><a href="?page=kuliner" class="text-white"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Kuliner</a></li>
+			<li><a href="?page=galeri_kuliner" class="text-white"><span class="glyphicon glyphicon-camera"></span>&nbsp;Galeri Kuliner</a></li>
+			<li><a href="?page=kontak" class="text-white"><span class="glyphicon glyphicon-pushpin"></span>&nbsp;Kontak</a></li>
+			<li><a href="?page=event" class="text-white"><span class="glyphicon glyphicon-bookmark"></span>&nbsp;Event</a></li>
+		</ul>
+	</div>
+
+	<div class="col-md-2 col-sm-3 col-xs-3">
+		<ul class="nav nav-pills">
+			<li class="dropdown ">
+				<a href="#" data-toggle="dropdown" data-target="#" class="text-white">
+					Sign In<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+					<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>&nbsp;Login</a></li>
+					<li><a href="daftar.php"><span class="glyphicon glyphicon-pencil"></span>&nbsp;Daftar</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
