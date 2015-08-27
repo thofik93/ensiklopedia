97486994cda38d9c03d9f17221801cc3bfc077b7
thofik93 <thofikwiranata@ymail.com>
2015-08-27 12:10:55 +0700

    interface menu halaman utama

---
+++ admin/pages/home.php
@@ -0,0 +1,71 @@
+<div class="row">
+    <div class="col-md-4">
+        <div class="thumbnail">
+            <div class="caption">
+                <h3>Kuliner Management</h3>
+                <p>Halaman untuk menambah, mengubah dan menghapus data kuliner</p>
+                <p><a href="?page=kuliner" class="btn btn-success">Masuk</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="thumbnail">
+            <div class="caption">
+                <h3>Kategori Management</h3>
+                <p>Halaman untuk menambah, mengubah dan menghapus data kategori</p>
+                <p><a href="?page=kategori" class="btn btn-success">Masuk</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="thumbnail">
+            <div class="caption">
+                <h3>Daerah Management</h3>
+                <p>Halaman untuk menambah, mengubah dan menghapus data kategori</p>
+                <p><a href="?page=daerah" class="btn btn-success">Masuk</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="thumbnail">
+            <div class="caption">
+                <h3>Member Evaluation</h3>
+                <p>Halaman untuk menghapus dan memblock data member</p>
+                <p><a href="?page=member" class="btn btn-success">Masuk</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="thumbnail">
+            <div class="caption">
+                <h3>Kritik dan Saran</h3>
+                <p>Halaman untuk melihat kritik dan saran dari para penggunjung website</p>
+                <p><a href="?page=kritik_saran" class="btn btn-success">Masuk</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="thumbnail">
+            <div class="caption">
+                <h3>Admin Management</h3>
+                <p>Halaman untuk menambah, dan menghapus data admin</p>
+                <p><a href="?page=admin" class="btn btn-success">Masuk</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="thumbnail">
+            <div class="caption">
+                <h3>Event Management</h3>
+                <p>Halaman untuk menambah, mengubah dan menghapus informasi event provinsi banten</p>
+                <p><a href="?page=event" class="btn btn-success">Masuk</a></p>
+            </div>
+        </div>
+    </div>
+</div>
