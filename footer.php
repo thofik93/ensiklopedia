940981b88786eb5c5156bf4babfd8f66af98219a
thofik93 <thofikwiranata@ymail.com>
2015-08-27 11:32:29 +0700

    bagian footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="container bg-footer footer">
+	<div class="row">
+		<div class="col-md-12">
+			<p class="text-white">2015 - Now © Copyright Ensiklopedia Kuliner Banten versi beta by <a href="https://twitter.com/thofik93" style="color: white" target="_blank">@thofik93</a> &nbsp;|&nbsp;  All Rights Reserved</p>
+		</div>
+		<div class="col-md-2">
+			<a href="https://twitter.com/bantenkuliner" id="twitter" target="_blank" >Follow Us On Twitter</a>
+		</div>
+		<div class="col-md-4">
+			<a href="https://www.facebook.com/kuliner.banten.5" id="facebook" class="text-white" target="_blank">Follow Us On Facebook</a>
+		</div>
+	</div>
+</div>
